1427e095c68f1f6707cc9fa2138d74e0355800ec
Gokul <gokul12cool@gmail.com>
2022-11-01 13:34:04 +0400

    inital commit

---
+++ py_procedure.py
@@ -0,0 +1,52 @@
+from asyncio import exceptions
+import pandas as pd
+
+def run(session, from_table, join_table, to_table):
+    try:
+        cust_table = session.sql(f"""INSERT INTO {to_table}
+                                            (
+                                                CUSTOMER_ID,         
+                                                DATE,                
+                                                SALUTATION,          
+                                                FIRST_NAME,          
+                                                LAST_NAME,
+                                                DATE_INSERT
+                                            )
+                                     SELECT C_CUSTOMER_SK
+                                            , TO_CHAR(D_DATE, 'YYYYMMDD')
+                                            , C_SALUTATION
+                                            , C_FIRST_NAME
+                                            , C_LAST_NAME
+                                            , CURRENT_TIMESTAMP()
+                                            FROM {from_table} C
+                                            JOIN {join_table} D 
+                                            ON D_DATE_SK = C_FIRST_SHIPTO_DATE_SK
+                                            LIMIT 100""").collect()
+                        
+        cust_table_2 = session.sql(f"""INSERT INTO {to_table}
+                                            (
+                                                CUSTOMER_ID,         
+                                                DATE,                
+                                                SALUTATION,          
+                                                FIRST_NAME,          
+                                                LAST_NAME,
+                                                DATE_INSERT
+                                            )
+                                    SELECT C_CUSTOMER_SK
+                                                , TO_CHAR(D_DATE, 'YYYYMMDD')
+                                                , C_SALUTATION
+                                                , C_FIRST_NAME
+                                                , C_LAST_NAME
+                                                , CURRENT_TIMESTAMP()
+                                    FROM {from_table} C
+                                    JOIN {join_table} D 
+                                    ON D_DATE_SK = C_FIRST_SHIPTO_DATE_SK
+                                    LIMIT 100""").collect()
+        
+        for i,j in zip(cust_table, cust_table_2):
+            print(i,j)
+
+    except exceptions:
+        print("Idk some error")
+                    
+    return i,j
