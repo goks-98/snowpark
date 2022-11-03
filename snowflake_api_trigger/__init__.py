f9c49caa6cb5847383a3613a79d8113bedd2682a
Gokul <gokul12cool@gmail.com>
2022-11-03 12:05:13 +0400

    Az Functions

---
+++ snowflake_api_trigger/__init__.py
@@ -0,0 +1,54 @@
+import logging
+import os
+from snowflake.snowpark import Session
+import requests
+import pandas as pd
+import json
+import azure.functions as func
+
+#Connecting to snowflake
+connection_parameters = {
+"account": "ld77469.uae-north.azure",
+"user": "GOKS98",
+"password": "March20311",
+"role": "ACCOUNTADMIN",
+"warehouse": "SNOWFLAKE_WAREHOUSE",
+"database": "OIL_AND_GAS",
+"schema": "CONFORMED"
+ }
+
+session = Session.builder.configs(connection_parameters).create()
+
+def main(req: Session) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    #Creating a Dataframe
+    df_table = session.table("SNOWFLAKE_SAMPLE_DATA.TPCDS_SF10TCL.WAREHOUSE")
+
+    #Converting to pandas df to make manipulation easier
+    df_pd = df_table.to_pandas()
+
+    #Getting weather data from API for every zipcode
+    json_list = []
+    try:
+        for i in df_pd['W_ZIP']:
+            response = requests.get(f"http://api.openweathermap.org/geo/1.0/zip?zip={i},US&appid=b3effbd4d36024a4b9735d33830e7ca3")
+            data = response.json()
+            json_list.append(data)
+    except ValueError:
+        pass
+    json_list
+
+    #Creating a new df with weather info and zip code
+    zip_code = df_pd['W_ZIP']
+    data = list(zip(zip_code, json_list))
+    weather_df = pd.DataFrame(data=data, columns=['W_ZIP', 'WEATHER_JSON'])
+    
+    #Joining weather nad warehouse tables
+    snowflake_df = df_pd.merge(weather_df, how='inner', on='W_ZIP')
+
+    snowflake_df_1 = session.create_dataframe(snowflake_df)
+    #Writing table to snowflake
+    snowflake_df_1.write.mode("overwrite").save_as_table("w_warehouse_weather")
+
+    return func.HttpResponse("Success !", status_code=200)
+    
